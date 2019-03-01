e986266312518d207a90321a34e15c107cf62b8e
Leo Tenenbaum <leonardomtenenbaum@gmail.com>
2019-03-01 09:15:49 -0500

    Re-writing NameGenerator

--- src/NameGenerator.h
+++
@@ -1,8 +0,0 @@
-#ifndef NAMEGENERATOR_H
-#define NAMEGENERATOR_H
-
-extern void  initialize_namegenerator();
-extern char* generate_name();
-extern char* generate_name_with_length(int min_length, int max_length);
-
-#endif
